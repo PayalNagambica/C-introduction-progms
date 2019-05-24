9b7104d0970252fa6e34375d48cb6cfa54f8d727
PayalNagambica <42710368+PayalNagambica@users.noreply.github.com>
2019-05-24 14:22:53 +0530

    Add files via upload

---
+++ pattern5.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+main ()
+{
+  printf ("C\n");
+  printf ("\tO\n");
+  printf ("\t\tM\n");
+  printf ("\t\t\tP\n");
+  printf("\t\t\t\tU\n");
+  printf("\t\t\t\t\tT\n");
+  printf("\t\t\t\t\t\tE\n");
+  printf("\t\t\t\t\t\t\tR");
+}
---
+++ pattern6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main ()
+{
+printf("\t\t\t\t\t\t\tC\n");
+printf("\t\t\t\t\t\tO\n");
+printf("\t\t\t\t\tM\n");
+printf("\t\t\t\tP\n");
+printf("\t\t\tU\n");
+printf("\t\tT\n");
+printf("\tE\n");
+printf("R\n");
+}
